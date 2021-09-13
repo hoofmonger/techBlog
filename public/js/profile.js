ac95e5ca1f2808d079822adfdecc4ce3334b811a
James Hoofard <james.hoofard.iii@gmail.com>
2021-09-13 12:27:51 -0500

    changed some front side things, currently testing changes

--- public/js/profile.js
+++ public/js/profile.js
@@ -2,7 +2,7 @@ const newFormHandler = async (event) => {
   event.preventDefault();
 
   const name = document.querySelector('#blog-name').value.trim();
-  const needed_funding = document.querySelector('#blog-funding').value.trim();
+  // const needed_funding = document.querySelector('#blog-funding').value.trim();
   const description = document.querySelector('#blog-desc').value.trim();
 
   if (name && needed_funding && description) {
